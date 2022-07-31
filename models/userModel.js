5d7f71a2b4644d7210572f39e8c4446c772983e1
Chanukya-hitman <revathipathiyarabati@gmail.com>
2022-08-01 00:38:11 +0530

    updated one

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,18 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     username : {type:String , required:true},
-    password : {type:String , required:true} 
+    password : {type:String , required:true},
+    firstName:{type:String , default:""},
+    lastName:{type:String , default:""},
+    email:{type:String , default:""},
+    mobileNumber:{type:String , default:""},
+    linkedIn:{type:String , default:""},
+    objective:{type:String , default:""},
+    address:{type:String , default:""},
+    education :{type:Array,default:[]},
+    skills:{type:Array,default:[]},
+    experience:{type:Array,default:[]},
+    projects:{type:Array,default:[]},
 } , {
     timestamps:true
 })
