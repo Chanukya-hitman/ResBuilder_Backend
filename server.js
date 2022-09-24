2ea27f41a16369608396efcbf6f2e3379e8dbd97
Chanukya-hitman <revathipathiyarabati@gmail.com>
2022-09-24 13:25:48 +0530

    env port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const dbConnect = require('./dbConnect')
 
 app.use(express.json())
 
-const port = process.env.port ||5000
+const port = process.env.PORT ||5000
 
 const userRoute = require('./routes/userRoute')
 
